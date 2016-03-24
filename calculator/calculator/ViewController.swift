401d44392008d8922fcc0c9692d940c1a0e67b67
moxiaoyuan <hqq1@admindeiMac-32.local>
2016-03-24 11:52:31 +0800

    action
    add action

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func num0(sender: UIButton) {
+        
+    }
+    @IBAction func num1(sender: UIButton) {
+    }
+    @IBAction func num2(sender: UIButton) {
+    }
+    @IBAction func num3(sender: UIButton) {
+    }
+    @IBAction func num4(sender: UIButton) {
+    }
+    @IBAction func num5(sender: UIButton) {
+    }
+    @IBAction func num6(sender: UIButton) {
+    }
+    @IBAction func num7(sender: UIButton) {
+    }
+    @IBAction func num8(sender: UIButton) {
+    }
+    @IBAction func num9(sender: UIButton) {
+    }
+    @IBAction func point(sender: UIButton) {
+    }
+    @IBAction func add(sender: UIButton) {
+    }
+    @IBAction func jian(sender: UIButton) {
+    }
+    @IBAction func cheng(sender: UIButton) {
+    }
+    @IBAction func chu(sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
